ad564cce4265ca44a124be478380ea4012122cfe
PopinaBudapest <popinabp@gmail.com>
2025-06-23 13:50:28 +0200

    New Google Scripts Deployment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   sheetUrl: "https://docs.google.com/spreadsheets/d/1R6FYoFKZjfj3U4LzOiN7F7L7lWZk1ploO3hM3VfOAFs/edit?rm=demo",
-  apiUrl: "https://script.google.com/macros/s/AKfycbxfcWh4M_BTxe-x6bAlVlFAH7pVTsx8_tT__hr60PyuX16xROFTXeCCLNnLE9eAe9VnVQ/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxMzeEmI4B6fMJVHE_x1egdMEkRYWrtLOBkcPj4jU1I6ucYGmPiI17Y66jscvbcbVBMYQ/exec"
 };
